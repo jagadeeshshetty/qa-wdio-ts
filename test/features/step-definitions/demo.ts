0df2541504f24ca7a080735d9894fbf15e33d17a
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2024-05-30 09:51:04 +0530

    async/await mode

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,7 +1,9 @@
 
 import { Given } from "@wdio/cucumber-framework";
 
-Given(/^I open the google search page$/, function () {
-  browser.url("https://www.google.com");
-  browser.debug();
+Given(/^I open the google search page$/, async function () {
+  console.log("🟡 Before open the google search page");
+  await  browser.url("https://www.google.com");
+  await browser.pause(5000);
+  console.log("🟡 After open the google search page");
 });
