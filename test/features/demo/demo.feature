8efbfec13850b527387ba4d45b8c753a6dcbf4f2
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2024-05-30 23:37:53 +0530

    implemented the "When I search with <searchText> keyword" step.

--- test/features/demo/demo.feature
+++ test/features/demo/demo.feature
@@ -3,6 +3,10 @@ Feature: Demo Feature
   Demo Feature Description
   Scenario Outline: Validate the google search result
     Given I open the google search page
-# When I search with <searchKeyword> keyword
-# And I click on the first search result
-# Then the URL should match <expectedURL>
+    When I search with <searchText> keyword
+    # And I click on the first search result
+    # Then the URL should match <expectedURL>
+
+    Examples:
+      | testcaseID | searchText | expUrl  |
+      | DEMO_0001  | WDIO       | Value 3 |
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,9 +1,17 @@
 
-import { Given } from "@wdio/cucumber-framework";
+import { Given, When } from "@wdio/cucumber-framework";
 
 Given(/^I open the google search page$/, async function () {
   console.log("🟡 Before open the google search page");
   await  browser.url("https://www.google.com");
-  await browser.pause(5000);
   console.log("🟡 After open the google search page");
 });
+
+When(/^I search with (.*) keyword$/, async function (searchText) {
+  const searchEditbox = await $('//textarea[@title="Search"]');
+  searchEditbox.setValue(searchText);
+  await browser.keys('Enter');
+  await browser.pause(5000);
+});
+// And I click on the first search result
+// Then the URL should match <expectedURL>
